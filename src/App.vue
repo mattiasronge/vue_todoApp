363ab27aa78e11a1e339c22426f289b1e6971264
Mattiasr <r.o.n.g.e@hotmail.com>
2019-02-26 13:50:30 +0100

    first
    first

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <section class="slider">
+<todos :todos="todos"/>
+<newtodo/>
+</section>
+  </div>
+</template>
+
+<script>
+
+import todos from './views/Todos'
+import newtodo from './views/NewTodo'
+
+export default {
+  name: 'app',
+  data(){
+    return{
+      todos: [
+        {done: false, text: 'köp bananer'},
+        {done: false, text: 'köp kex'},
+        {done: true, text: 'köp alkohol'}
+      ]
+    }
+  },
+  components: {
+    todos,
+    newtodo
+  }
+}
+
+</script>
+
+<style lang="scss">
+
+@import 'scss/main.scss';
+
+</style>
